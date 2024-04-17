09b1c7b99d2e3459068bba345501180dc932b484
BradiLugembaTUD <C22348001@mytudublin.ie>
2024-04-17 18:07:37 +0100

    Main code

---
+++ main.java
@@ -0,0 +1,13 @@
+public class main {
+    public static void main(String[] args) {
+        
+        // Program logic goes here
+        
+        // Example: Print "Hello, world!" to the console
+        System.out.println("Hello, world!");
+
+        String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new music());
+    }
+
+}
