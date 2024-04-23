598e93223c739b284838da8cd9edcdd58d268795
BradiLugembaTUD <C22348001@mytudublin.ie>
2024-04-23 19:21:16 +0100

    Creating the size of the screen and storing the music file in a variable

--- RespectVisualizer.java
+++ RespectVisualizer.java
@@ -4,5 +4,10 @@ import ddf.minim.analysis.*;
 
 public class RespectVisualizer extends PApplet {
 
-    
+    // Configuration variables
+    // ------------------------
+    int canvasWidth = 1080;
+    int canvasHeight = 1080;
+
+    String audioFileName = "data/respect.mp3"; // Audio file in data folder
 }
