2726061e4f7831eee87d8a546bb07f3ffa3fab71
BradiLugembaTUD <C22348001@mytudublin.ie>
2024-04-23 19:23:28 +0100

    These variables will help everything move smoother

--- RespectVisualizer.java
+++ RespectVisualizer.java
@@ -10,4 +10,8 @@ public class RespectVisualizer extends PApplet {
     int canvasHeight = 1080;
 
     String audioFileName = "data/respect.mp3"; // Audio file in data folder
+
+    float fps = 30;
+    float smoothingFactor = 0.25f; // FFT audio analysis smoothing factor
+    // ------------------------
 }
